01c2a502527d9f6e2d9d12d489b54f9a98b1d8f6
Lukas Taake <lukas.taake@gmail.com>
2016-10-25 12:23:44 +0200

    added maven and spring boot

---
+++ src/main/java/de/fh/bielefeld/NewsboardApplication.java
@@ -0,0 +1,12 @@
+package de.fh.bielefeld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsboardApplication.class, args);
+	}
+}
