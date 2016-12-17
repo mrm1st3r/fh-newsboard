91cdfdc1e709c45e01af422ae11a23b6b8bea9b1
Felix Meyer <fm@schiffel.it>
2016-12-17 12:18:41 +0100

    Added bean definitions to NewsboardApplication configuration class.

--- src/main/java/de/fh_bielefeld/newsboard/NewsboardApplication.java
+++ src/main/java/de/fh_bielefeld/newsboard/NewsboardApplication.java
@@ -1,5 +1,7 @@
 package de.fh_bielefeld.newsboard;
 
+import de.fh_bielefeld.newsboard.dao.*;
+import de.fh_bielefeld.newsboard.model.AuthenticationToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
@@ -9,6 +11,36 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 public class NewsboardApplication {
+
+	@Bean("prototype")
+	public AuthenticationTokenDao authenticationTokenDao() {
+		return new AuthenticationTokenDaoImpl();
+	}
+
+	@Bean("prototype")
+	public ClassificationDao classificationDao() {
+		return new ClassificationDaoImpl();
+	}
+
+	@Bean("prototype")
+	public DocumentDao documentDao() {
+		return new DocumentDaoImpl();
+	}
+
+	@Bean("prototype")
+	public ExternDocumentDao externDocumentDao() {
+		return new ExternDocumentDaoImpl();
+	}
+
+	@Bean("prototype")
+	public ExternModuleDao externModuleDao() {
+		return new ExternModuleDaoImpl();
+	}
+
+	@Bean("prototype")
+	public SentenceDao sentenceDao() {
+		return new SentenceDaoImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsboardApplication.class, args);
