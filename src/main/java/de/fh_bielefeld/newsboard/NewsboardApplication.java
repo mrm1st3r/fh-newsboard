29edaa49ca35a373ab56d0ab5a88814bdd1cf57a
Felix Meyer <fm@schiffel.it>
2016-12-21 16:59:33 +0100

    Removed unnecessary annotation componentscan in NewsboardApplication.

--- src/main/java/de/fh_bielefeld/newsboard/NewsboardApplication.java
+++ src/main/java/de/fh_bielefeld/newsboard/NewsboardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class NewsboardApplication {
 
 	public static void main(String[] args) {
