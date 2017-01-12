70ad11cb42c9269c14dd7856fbbf3894b892943a
Lukas Taake <lukas.taake@gmail.com>
2017-01-12 21:57:49 +0100

    Wired RestApiController for reading document list

--- src/main/java/de/fh_bielefeld/newsboard/rest/RestApiController.java
+++ src/main/java/de/fh_bielefeld/newsboard/rest/RestApiController.java
@@ -1,8 +1,12 @@
 package de.fh_bielefeld.newsboard.rest;
 
+import de.fh_bielefeld.newsboard.dao.DocumentDao;
+import de.fh_bielefeld.newsboard.model.Document;
 import de.fh_bielefeld.newsboard.model.RawDocument;
 import de.fh_bielefeld.newsboard.xml.XmlDocumentReader;
+import de.fh_bielefeld.newsboard.xml.XmlDocumentWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +15,10 @@ import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,13 +31,30 @@ import java.util.List;
 public class RestApiController {
 
     private XmlDocumentReader xmlReader;
+    private XmlDocumentWriter xmlWriter;
+    private DocumentDao documentDao;
 
-    public RestApiController(@Autowired XmlDocumentReader xmlReader) {
+    @Autowired
+    public RestApiController(XmlDocumentReader xmlReader, XmlDocumentWriter xmlWriter, DocumentDao documentDao) {
         this.xmlReader = xmlReader;
+        this.xmlWriter = xmlWriter;
+        this.documentDao = documentDao;
     }
 
-    @RequestMapping(path = "/document", method = RequestMethod.GET)
-    public String listDocuments() { return "Hello world"; }
+    @RequestMapping(path = "/document", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+    public String listDocuments(HttpServletResponse response) {
+
+        // TODO: Use real documents when database works
+        //List<Document> documents = documentDao.getAllDocumentsOnlyWithMetaData();
+        List<Document> documents = new ArrayList<>();
+        try {
+            return xmlWriter.writeStubList(documents);
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return "Internal Error: " + e.getMessage();
+        }
+    }
 
     @RequestMapping(path = "/document", method = RequestMethod.PUT)
     public void putDocument(HttpServletResponse response, @RequestBody String body) {
