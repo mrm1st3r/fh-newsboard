b8ab875a9cb84d77e59c7c900b3959f584c23316
Lukas Taake <lukas.taake@gmail.com>
2017-01-16 13:06:32 +0100

    Added iterating documents and displaying titles on start page

--- src/main/java/de/fh_bielefeld/newsboard/frontend/FrontendController.java
+++ src/main/java/de/fh_bielefeld/newsboard/frontend/FrontendController.java
@@ -1,7 +1,14 @@
 package de.fh_bielefeld.newsboard.frontend;
 
+import de.fh_bielefeld.newsboard.dao.DocumentDao;
+import de.fh_bielefeld.newsboard.model.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Controller for frontend requests
@@ -9,8 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
+    private static final long DOCUMENTS_PER_PAGE = 10;
+    private DocumentDao documentDao;
+
+    @Autowired
+    public FrontendController(DocumentDao documentDao) {
+        this.documentDao = documentDao;
+    }
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Document> documentStubs = documentDao.getAllDocumentsOnlyWithMetaData();
+        List<Document> documents = documentStubs.stream().limit(DOCUMENTS_PER_PAGE).map(
+                document -> documentDao.getDocumentWithId(document.getId())).collect(Collectors.toList());
+        model.addAttribute("documents", documents);
         return "index";
     }
 }
