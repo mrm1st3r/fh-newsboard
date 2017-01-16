73a69d776f86707669baccc968f90df0852ee3fe
Felix Meyer <felix.meyer@fh-bielefeld.de>
2017-01-16 21:00:57 +0100

    Updated AuthenticationTokenDaoTest to use new implemented autosetting of id.

--- src/test/groovy/de/fh_bielefeld/newsboard/dao/AuthenticationTokenDaoTest.groovy
+++ src/test/groovy/de/fh_bielefeld/newsboard/dao/AuthenticationTokenDaoTest.groovy
@@ -23,38 +23,46 @@ class AuthenticationTokenDaoTest extends Specification {
     @Autowired
     ExternModuleDao externModuleDao
 
+    private List<String> moduleIds
+    private List<Integer> authTokenIds
+
     def "test updating"() {
         setup:
         ExternModule additionalExternModule = getNewExternModule()
         additionalExternModule.setId("extern_module_testing_2")
         additionalExternModule.setAuthor("test_author_2")
         additionalExternModule.setName("Additional extern module for testing purpose")
-        externModuleDao.insertExternModule(additionalExternModule)
+        insertExternModule(additionalExternModule);
 
         when:
         AuthenticationToken token = getNewAuthenticationToken()
-        authenticationTokenDao.insertAuthenticationToken(token)
-        AuthenticationToken insertedToken = getLastInsertedToken()
-        insertedToken.setToken("ghi")
-        insertedToken.setModuleId("extern_module_testing_2")
+        insertToken(token)
+        token.setToken("ghi")
+        token.setModuleId("extern_module_testing_2")
 
         then:
-        authenticationTokenDao.updateAuthenticationToken(insertedToken)
-        AuthenticationToken testingToken = authenticationTokenDao.getAllTokenForModule(additionalExternModule).get(0)
-        testingToken.getToken() == "ghi"
+        authenticationTokenDao.updateAuthenticationToken(token)
+
+        AuthenticationToken testingToken = authenticationTokenDao.getTokenWithId(token.getId())
+        testingToken.getToken() == token.getToken()
+        testingToken.getId() == token.getId()
+        testingToken.getModuleId() == token.getModuleId()
+
         noExceptionThrown()
     }
 
     def "test selection with id"() {
         when:
         AuthenticationToken token = getNewAuthenticationToken()
-        authenticationTokenDao.insertAuthenticationToken(token)
-        AuthenticationToken referenceToken = getLastInsertedToken()
+        insertToken(token)
 
         then:
-        AuthenticationToken testingToken = authenticationTokenDao.getTokenWithId(referenceToken.getId())
-        testingToken.getModuleId() == referenceToken.getModuleId()
-        testingToken.getToken() == referenceToken.getToken()
+        AuthenticationToken testingToken = authenticationTokenDao.getTokenWithId(token.getId())
+
+        testingToken.getModuleId() == token.getModuleId()
+        testingToken.getToken() == token.getToken()
+        testingToken.getId() == token.getId()
+
         noExceptionThrown()
     }
 
@@ -62,39 +70,67 @@ class AuthenticationTokenDaoTest extends Specification {
         when:
         AuthenticationToken token = getNewAuthenticationToken()
         ExternModule module = getNewExternModule()
-        authenticationTokenDao.insertAuthenticationToken(token)
-        authenticationTokenDao.insertAuthenticationToken(token)
-        authenticationTokenDao.insertAuthenticationToken(token)
+        insertToken(token)
+        insertToken(token)
+        insertToken(token)
+        insertToken(token)
+        insertToken(token)
 
         then:
         List<AuthenticationToken> allTokens = authenticationTokenDao.getAllTokenForModule(module)
-        allTokens.size() == 3
+
+        allTokens.size() == 5
         for (AuthenticationToken t : allTokens) {
             t.getToken() == token.getToken()
             t.getModuleId() == token.getModuleId()
+            t.getId() == token.getId()
         }
+
         noExceptionThrown()
     }
 
     def "test insertion"() {
         when:
         AuthenticationToken tokenToInsert = getNewAuthenticationToken()
-        authenticationTokenDao.insertAuthenticationToken(tokenToInsert)
+        insertToken(tokenToInsert)
 
         then:
-        AuthenticationToken token = getLastInsertedToken()
+        AuthenticationToken token = authenticationTokenDao.getTokenWithId(tokenToInsert.getId())
+
         token.getModuleId() == tokenToInsert.getModuleId()
         token.getToken() == tokenToInsert.getToken()
+
         noExceptionThrown()
     }
 
     def setup() {
-        externModuleDao.insertExternModule(getNewExternModule())
+        moduleIds = new ArrayList<String>()
+        authTokenIds = new ArrayList<Integer>()
+
+        insertExternModule(getNewExternModule())
     }
 
     def cleanup() {
-        jdbcTemplate.update("DELETE FROM extern_module")
-        jdbcTemplate.update("DELETE FROM authentication_token")
+        for (Integer i : authTokenIds) {
+            jdbcTemplate.update("DELETE FROM authentication_token WHERE id = " + i)
+        }
+        for (String s : moduleIds) {
+            jdbcTemplate.update("DELETE FROM extern_module WHERE id = '" + s + "'")
+        }
+    }
+
+    def insertToken(AuthenticationToken token) {
+        authenticationTokenDao.insertAuthenticationToken(token)
+        authTokenIds.add(token.getId())
+    }
+
+    def insertExternModule(ExternModule module) {
+        externModuleDao.insertExternModule(module)
+        moduleIds.add(module.getId())
+    }
+
+    def getNewAuthenticationToken() {
+        return new AuthenticationToken(-1, "extern_module_test", "abc")
     }
 
     def getNewExternModule() {
@@ -103,13 +139,5 @@ class AuthenticationTokenDaoTest extends Specification {
         externModule.setAuthor("test_author")
         externModule.setName("Extern_Module for testing purpose")
         return externModule
-    }
-
-    def getNewAuthenticationToken() {
-        return new AuthenticationToken(-1, "extern_module_test", "abc")
-    }
-
-    def getLastInsertedToken() {
-        return authenticationTokenDao.getAllTokenForModule(getNewExternModule()).get(0)
     }
 }
