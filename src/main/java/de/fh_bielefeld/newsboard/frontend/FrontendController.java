681b85347f660fb9a164ceae5c3da1ccb2f86aee
Lukas Taake <lukas.taake@gmail.com>
2017-01-17 12:22:26 +0100

    Added detail page to show more information about a specified document

--- src/main/java/de/fh_bielefeld/newsboard/frontend/FrontendController.java
+++ src/main/java/de/fh_bielefeld/newsboard/frontend/FrontendController.java
@@ -5,6 +5,7 @@ import de.fh_bielefeld.newsboard.model.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,5 +32,12 @@ public class FrontendController {
                 document -> documentDao.getDocumentWithId(document.getId())).collect(Collectors.toList());
         model.addAttribute("documents", documents);
         return "index";
+    }
+
+    @RequestMapping(value = "/document/{id}")
+    public String details(Model model, @PathVariable String id) {
+        Document doc = documentDao.getDocumentWithId(Integer.parseInt(id));
+        model.addAttribute("doc", doc);
+        return "detail";
     }
 }
