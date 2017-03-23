383edf74018cfdf1156208aab59deacbc62ae205
Lukas Taake <lukas.taake@gmail.com>
2017-03-23 12:47:15 +0100

    Removed authentication_token and added access and access_role tables

--- src/main/resources/sql/create_script.sql
+++ src/main/resources/sql/create_script.sql
@@ -1,85 +1,117 @@
-CREATE TABLE IF NOT EXISTS extern_document (
-  id        INT         NOT NULL AUTO_INCREMENT,
-  title     VARCHAR(50) NOT NULL,
-  html      LONGTEXT    NOT NULL,
-  module_id VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id)
+/*
+ * Create Tables
+ */
+
+CREATE TABLE IF NOT EXISTS external_document (
+  ext_document_id INT         NOT NULL AUTO_INCREMENT,
+  title           VARCHAR(50) NOT NULL,
+  html            LONGTEXT    NOT NULL,
+  module_id       VARCHAR(50) NOT NULL,
+  PRIMARY KEY(ext_document_id)
 );
 
-CREATE TABLE IF NOT EXISTS extern_module (
-  id          VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS module (
+  module_id   VARCHAR(50) NOT NULL,
+  access_id   VARCHAR(30) NOT NULL,
   name        VARCHAR(50) NOT NULL,
   author      VARCHAR(50) NOT NULL,
   description TEXT        NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (module_id)
 );
 
-CREATE TABLE IF NOT EXISTS authentication_token (
-  id        INT         NOT NULL AUTO_INCREMENT,
-  module_id VARCHAR(50) NOT NULL,
-  token     VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS access (
+  access_id   VARCHAR(30)   NOT NULL,
+  role_id     VARCHAR(20)   NOT NULL,
+  passphrase  VARCHAR(255)  NOT NULL,
+  hash_type   VARCHAR(10)   NOT NULL,
+  enabled     BOOLEAN       NOT NULL,
+  PRIMARY KEY(access_id)
+);
+
+CREATE TABLE IF NOT EXISTS access_role (
+  role_id     VARCHAR(20) NOT NULL,
+  PRIMARY KEY (role_id)
 );
 
 CREATE TABLE IF NOT EXISTS sentence (
-  id          INT   NOT NULL AUTO_INCREMENT,
+  sentence_id INT   NOT NULL AUTO_INCREMENT,
   number      INT   NOT NULL,
   text        TEXT  NOT NULL,
   document_id INT   NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(sentence_id)
 );
 
 CREATE TABLE IF NOT EXISTS document (
-  id            INT         NOT NULL AUTO_INCREMENT,
+  document_id   INT         NOT NULL AUTO_INCREMENT,
   title         VARCHAR(50) NOT NULL,
   author        VARCHAR(50) NULL,
   source        TINYTEXT    NOT NULL,
   creation_time DATETIME    NULL,
   crawl_time    DATETIME    NOT NULL,
   module_id     VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(document_id)
 );
 
 CREATE TABLE IF NOT EXISTS classification (
-  sent_id     INT             NOT NULL,
+  sentence_id INT             NOT NULL,
   module_id   VARCHAR(50)     NOT NULL,
   value       DECIMAL(13, 10) NOT NULL,
   confidence  DECIMAL(13, 10) NULL,
-  PRIMARY KEY (sent_id, module_id)
+  PRIMARY KEY (sentence_id, module_id)
 );
 
-ALTER TABLE extern_document
-  ADD CONSTRAINT fk_extern_document_extern_module
+/*
+ * Add foreign keys
+ */
+
+ALTER TABLE external_document
+  ADD CONSTRAINT fk_external_document_module
   FOREIGN KEY (module_id)
-  REFERENCES extern_module (id)
+  REFERENCES module (module_id)
   ON DELETE RESTRICT;
 
-ALTER TABLE authentication_token
-  ADD CONSTRAINT fk_authentication_token_extern_module
-  FOREIGN KEY (module_id)
-  REFERENCES extern_module (id)
+ALTER TABLE module
+  ADD CONSTRAINT fk_module_access
+  FOREIGN KEY (access_id)
+  REFERENCES access (access_id)
   ON DELETE CASCADE;
+
+ALTER TABLE access
+  ADD CONSTRAINT fk_access_access_role
+  FOREIGN KEY (role_id)
+  REFERENCES access_role (role_id)
+  ON DELETE RESTRICT;
 
 ALTER TABLE sentence
   ADD CONSTRAINT fk_sentence_document
   FOREIGN KEY (document_id)
-  REFERENCES document (id)
+  REFERENCES document (document_id)
   ON DELETE CASCADE;
 
 ALTER TABLE document
-  ADD CONSTRAINT fk_document_extern_module
+  ADD CONSTRAINT fk_document_module
   FOREIGN KEY (module_id)
-  REFERENCES extern_module (id)
+  REFERENCES module (module_id)
   ON DELETE RESTRICT;
 
 ALTER TABLE classification
   ADD CONSTRAINT fk_classification_sentence
   FOREIGN KEY (sent_id)
-  REFERENCES sentence (id)
+  REFERENCES sentence (sentence_id)
   ON DELETE CASCADE;
 
 ALTER TABLE classification
-  ADD CONSTRAINT fk_classification_extern_module
+  ADD CONSTRAINT fk_classification_module
   FOREIGN KEY (module_id)
-  REFERENCES extern_module (id)
+  REFERENCES module (module_id)
   ON DELETE RESTRICT;
+
+/*
+ * Add base data
+ */
+
+INSERT INTO access_role (role_id) VALUES
+  ('crawler'),
+  ('classifier'),
+  ('external-source'),
+  ('admin-user');
