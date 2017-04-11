7a806f9f4e9566d5fadabb9620299962f12010ec
Lukas Taake <lukas.taake@gmail.com>
2017-04-11 13:57:20 +0200

    Added basic tests for frontend controller

---
+++ src/test/groovy/de/fhbielefeld/newsboard/frontend/FrontendControllerTest.groovy
@@ -0,0 +1,36 @@
+package de.fhbielefeld.newsboard.frontend
+
+import de.fhbielefeld.newsboard.dao.DocumentDao
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FrontendControllerTest extends Specification {
+
+    @Autowired
+    MockMvc mvc
+
+    @Autowired
+    DocumentDao documentDao
+
+    def "should access start page"() {
+        expect:
+        mvc.perform(get("/")).andExpect(status().isOk())
+    }
+
+    def "should access detail page"() {
+        when:
+        def docs = documentDao.findAllStubs()
+        int id = docs[0].getId()
+
+        then:
+        mvc.perform(get("/document/" + id)).andExpect(status().isOk())
+    }
+}
