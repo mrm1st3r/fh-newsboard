dd8608866dce267244bdb35aa0b146ca027f5c49
Lukas Taake <lukas.taake@gmail.com>
2017-05-19 14:52:02 +0200

    Created ClassificationValue

---
+++ src/main/java/de/fhbielefeld/newsboard/model/ClassificationValue.java
@@ -0,0 +1,53 @@
+package de.fhbielefeld.newsboard.model;
+
+import de.smartsquare.ddd.annotations.DDDValueObject;
+
+/**
+ * A single classification value consisting of
+ * a classification value and an optional confidence.
+ */
+@DDDValueObject
+public class ClassificationValue {
+
+    private static final double DEFAULT_CONFIDENCE = 1;
+
+    private static final ClassificationValue POSITIVE = new ClassificationValue(1, DEFAULT_CONFIDENCE);
+    private static final ClassificationValue NEUTRAL = new ClassificationValue(0, DEFAULT_CONFIDENCE);
+    private static final ClassificationValue NEGATIVE = new ClassificationValue(-1, DEFAULT_CONFIDENCE);
+
+    private final double value;
+    private final double confidence;
+
+    public static ClassificationValue of(double value) {
+        if (value == 0) {
+            return NEUTRAL;
+        } else if (value == 1) {
+            return POSITIVE;
+        } else if (value == -1) {
+            return NEGATIVE;
+        }
+        return new ClassificationValue(value, DEFAULT_CONFIDENCE);
+    }
+
+    public static ClassificationValue of(double value, double confidence) {
+        if (confidence == DEFAULT_CONFIDENCE) {
+            return of(value);
+        }
+        return new ClassificationValue(value, confidence);
+    }
+
+    private ClassificationValue(double value, double confidence) {
+        if (value < -1 || value > 1) {
+            throw new IllegalArgumentException("Classification value must be in range [-1 .. 1]");
+        }
+        if (confidence < 0 || confidence > 1) {
+            throw new IllegalArgumentException("Classification confidence must be in range [0 .. 1]");
+        }
+        this.value = value;
+        this.confidence = confidence;
+    }
+
+    public double effectiveValue() {
+        return value * confidence;
+    }
+}
---
+++ src/test/groovy/de/fhbielefeld/newsboard/model/ClassificationValueTest.groovy
@@ -0,0 +1,60 @@
+package de.fhbielefeld.newsboard.model
+
+import spock.lang.Specification
+
+class ClassificationValueTest extends Specification {
+
+    def "should use default instances"() {
+        when:
+        def classification = ClassificationValue.of(value)
+
+        then:
+        classification.is(instance)
+
+        where:
+        value   | instance
+        1       | ClassificationValue.POSITIVE
+        0       | ClassificationValue.NEUTRAL
+        -1      | ClassificationValue.NEGATIVE
+    }
+
+    def "should calculate effectiveValue"() {
+        given:
+        def classification = ClassificationValue.of(value, confidence)
+
+        expect:
+        classification.effectiveValue() == effective
+
+        where:
+        value   | confidence    || effective
+        1       | 1             || 1
+        1       | 0             || 0
+        0.5     | 1             || 0.5
+        0.5     | 0.5           || 0.25
+
+    }
+
+    def "should not accept classification value out of range"() {
+        when:
+        ClassificationValue.of(value)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        value << [-2d, 2d, 7d, 42d, -1.1d, 1.2d]
+    }
+
+    def "should not accept confidence out of range"() {
+        when:
+        ClassificationValue.of(0.5,confidence)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        confidence << [2.1d, 1.1d, -0.1d, -7d]
+    }
+
+
+}
